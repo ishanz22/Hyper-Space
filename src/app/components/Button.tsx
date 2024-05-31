7d16fa0d8cc50dd370e9168a830359d5172cab59
Mohomed Ahsan <ishanzloro22@gmail.com>
2024-05-31 11:50:48 +0400

    Updated server component popup

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -2,13 +2,19 @@ import Link from "next/link";
 import React from "react";
 
 const Button = ({ href, color, children }) => {
+  const colorClasses = {
+    blue: "bg-blue-500 hover:bg-blue-700",
+    red: "bg-red-500 hover:bg-red-700",
+    green: "bg-green-500 hover:bg-green-700",
+  };
+
+  const buttonColor = colorClasses[color] || "bg-gray-500 hover:bg-gray-700";
+
   return (
     <Link href={href}>
-    <div className={`bg-${color}-500 hover:bg-${color}-700 font-bold py-3 px-6 rounded-full transition duration-300 ease-in-out transform hover:scale-105 text-white`}>
-     
+      <div className={`font-bold py-3 px-6 rounded-full transition duration-300 ease-in-out transform hover:scale-105 text-white ${buttonColor}`}>
         <div>{children}</div>
-   
-    </div>
+      </div>
     </Link>
   );
 };
